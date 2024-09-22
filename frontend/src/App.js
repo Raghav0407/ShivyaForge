1cceb1eb571e3a27c05df10e9352bac72c5881da
Awan <awanyadav2002@gmail.com>
2024-09-22 19:20:38 +0530

    Added Component And Parts , GT And LN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
+
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Footer/>
+
     </div>
   );
 }

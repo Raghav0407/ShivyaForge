0e482b08b9789cc639c62af2e7b7a5e4b252478b
Awan <awanyadav2002@gmail.com>
2024-09-26 20:01:18 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import ComponentAndParts from "./Components/ComponentAndParts";
 
 function App() {
   return (
     <div className="App">
-
+      <ComponentAndParts />
     </div>
   );
 }

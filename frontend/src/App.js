b440d5b1a33dcef4a5871030629582a281095731
Raghav Garg <raghavgarg4702@gmail.com>
2024-09-30 21:49:57 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,26 @@ import './App.css';
 import Poster from './Components/Poster';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import ContactPage from './Pages/ContactPage';
 import AboutPage from './Pages/AboutPage';
 import ProductPage from './Pages/ProductPage';
 import HomePage from './Pages/HomePage';
 function App() {
- 
+
   return (
     <div className="App">
-        <BrowserRouter>
-       <Navbar/>
-       <Routes>
-        <Route path='/contactus' element={<ContactPage/>}/>
-        <Route path='/aboutus' element={<AboutPage/>}/>
-        <Route path='/productshow' element={<ProductPage/>}/>
-        <Route path='/home' element={<HomePage/>}/>
-       </Routes>
-       <Poster/>
-       <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/contactus' element={<ContactPage />} />
+          <Route path='/aboutus' element={<AboutPage />} />
+          <Route path='/productshow' element={<ProductPage />} />
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+        <Poster />
+        <Footer />
       </BrowserRouter>
     </div>
   );

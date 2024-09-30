1eb65a5b41a73f4acbcbd00d24e524726cf4db73
parwejkhan1816 <pkhan181611786@gmail.com>
2024-09-30 21:32:37 +0530

    update card.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Aboutus from './Components/Aboutus';
+import CardComponent from './Components/Card';
 
 function App() {
  
   return (
     <div className="App">
+      <Aboutus />
+      <CardComponent />
     </div>
   );
 }

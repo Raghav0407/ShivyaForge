64abdcb7047f37854938929f64c5fb74078d6444
Awan <awanyadav2002@gmail.com>
2024-10-01 20:46:30 +0530

    Added some components

--- frontend/src/Components/Parts.jsx
+++ frontend/src/Components/Parts.jsx
@@ -1,17 +1,19 @@
 import parts from '../Data/PartsImage.json';
 import "../Style/GeneralStyling.css"
+import {useEffect, useState} from "react";
 
 const Parts = () => {
+
     return (
         <div className="container">
-            <div className="h2 text-center mt-5 mb-3 text-decoration-underline text-secondary custom-font">
-                --Parts--
+            <div className="h2 text-center mt-5 p-5 bg-light text-secondary custom-font">
+                Parts
             </div>
             <div className="parts_container p-6">
                 <div className="row">
                     {parts.Parts.map((item) => (
                         <div className="col-lg-3 col-md-4 col-sm-6 mb-3 image-zoom">
-                            <img className="img-fluid img-block rounded-2 shadow" src={item.Url} alt="part-image"/>
+                            <img className="img-fluid img-block rounded-2 shadow" src={`assets/Machined_Forged_Parts/${item.Url}`} alt="part-image"/>
                         </div>
                     ))}
                 </div>

97796a20fd2c0b3d264360741ca982d95d14b4c6
Raghav Garg <raghavgarg4702@gmail.com>
2024-10-13 19:12:40 +0530

    Changes

---
+++ frontend/src/Components/NewAboutUs.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import '../Styles/NewAboutUs.css';
+
+export const NewAboutUs = () => {
+    return (
+        
+        <div className='NewAboutUs-parent' style={{border:"2px solid orange",width:"90%",margin:"auto",marginTop:"20px",marginBottom:"20px"}}>
+            <div className='NewAboutUs-first'>
+                <h3>About Us</h3>
+            </div>
+            <div className='NewAboutUs-second'>
+                <p>We, <b>SHIVYA FORGE</b>, are a Leading <b>Manufacturer</b> and <b>Exporter</b> of <b>Forged Rings, Forged Components</b> and <b>Machined Forged Parts in
+                    Gujarat, INDIA.</b><br /> <br />
+                    The Company Management Team is having 32+ years of experience in Forging Industry.<br /> <br />
+                    Our Customized Forging Products are made using best quality raw materials and modern technology. Raw Material Grades are
+                    Stainless Steel, Carbon Steel, Alloy Steel and Other Metals.<br /> <br />
+                    Having strong knowledge and experience in Commercial and Technical Areas, we are able to Manufacture and Export Various
+                    Types of <b>Quality Forging Products</b> as per <b>Customer’s Drawings</b> and <b>Specifications</b> at <b>Best Competitive Prices</b></p>
+            </div>
+            <div className='NewAboutUs-third'>
+
+                <div className='NewAboutUs-third-a'>
+                    <h4 className="facilities-heading">
+                        Facilities
+                    </h4>
+                    <ul>
+                        <li>Cutting Shop</li>
+                        <li>Die Shop</li>
+                        <li>Open Die Forging</li>
+                        <li>Closed Die Forging</li>
+                        <li>Ring Rolling Forging</li>
+                        <li>Heat Treatment Facility</li>
+                        <li>Annealing Facility</li>
+                        <li>Machine Shop</li>
+                        <li>Testing Facility</li>
+                    </ul>
+                </div>
+
+
+                <div className='NewAboutUs-third-b'></div>
+
+                <div className='NewAboutUs-third-c'>
+
+                    <div style={{ height: 'auto' }}>
+                        <h4 style={{ marginLeft: "10px", color: "white", background: "orange", padding: "5px" }}>
+                            Qualities Assurance
+                        </h4>
+                        <p style={{ marginLeft: "10px" }}>At <b>SHIVYA FORGE</b>, we Manufacture and
+                            Export <b>Quality Products</b> as per Customers Industrial
+                            requirements. We believe in ultimate customer
+                            satisfaction, in terms of <b>Quality</b> and <b>Delivery.</b>
+                        </p>
+                    </div>
+
+                    <div style={{ width: '100%', height: 'auto' }}>
+                        <h4 style={{ marginLeft: "10px", color: "white", background: "orange", padding: "5px" }}>
+                            Our Values
+                        </h4>
+                        <p style={{ marginLeft: "10px" }}>At <b>SHIVYA FORGE</b>, we Manufacture and
+                            Export <b>Quality Products</b> as per Customers Industrial
+                            requirements. We believe in ultimate customer
+                            satisfaction, in terms of <b>Quality</b> and <b>Delivery.</b>
+                        </p>
+                    </div>
+
+                </div>
+
+
+                <div className='NewAboutUs-third-d'></div>
+
+                <div className='NewAboutUs-third-a'>
+                    <h4 className="facilities-heading">
+                        Facilities
+                    </h4>
+                    <ul>
+                        <li>Automotive</li>
+                        <li>Agricultural Machinery</li>
+                        <li>Industrial Vehicles</li>
+                        <li>Bearings</li>
+                        <li>Gears & Transmission</li>
+                        <li>Oil and Gas</li>
+                        <li>Power</li>
+                        <li>Iron & Stee</li>
+                        <li>Mechanical Industries</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
